383d1d4c10e5dab529337d64ddc9ba646e8fab10
JordanBeziau <jordanbeziau@gmail.com>
2017-07-10 16:42:37 +0200

    namespace

---
+++ POO/namespace/B/infoB.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 10/07/2017
+ * Time: 16:35
+ */
+
+namespace B;
+
+class Info {
+
+  public function __construct() {
+    echo "Une classe " . __NAMESPACE__ . "<br>";
+  }
+
+}
---
+++ POO/namespace/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 10/07/2017
+ * Time: 16:31
+ */
+
+require "A/infoA.php";
+require "B/infoB.php";
+
+$a = new \A\Info();
+$b = new \B\Info();
