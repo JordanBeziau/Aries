28e07cabc57c3b0a6a9f10f4ca96736e085c50ef
JordanBeziau <jordanbeziau@gmail.com>
2017-07-11 14:58:57 +0200

    mvc setting helpers + rooter

---
+++ mvc/application/controllers/Accueil.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 11/07/2017
+ * Time: 14:44
+ */
+class Accueil {
+
+  /**
+   * méthode par défaut
+   */
+  public function index() {
+    vd("Controlleur Accueil");
+  }
+
+  /**
+   * Affiche numéro
+   * @param $_num
+   */
+  public function affiche($_num) {
+
+  }
+
+}
---
+++ mvc/application/controllers/Erreur.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 11/07/2017
+ * Time: 14:49
+ */
+class Erreur {
+
+  /**
+   * méthode par défaut
+   */
+  public function index() {
+    echo "Controlleur Erreur";
+  }
+
+}
