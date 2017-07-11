0d3c325a3fa91846e26d089d633f614b90d87bb8
JordanBeziau <jordanbeziau@gmail.com>
2017-07-11 11:14:27 +0200

    Composer

---
+++ Composer/PremierProjet/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 11/07/2017
+ * Time: 10:54
+ */
+
+require "vendor/autoload.php";
+
+$faker = Faker\Factory::create("fr_FR");
+
+$person = $faker->address;
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>'.$person.'</h1>');
+$mpdf->Output();
---
+++ Composer/PremierProjet/lib/helpers/debug.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jordanbeziau
+ * Date: 11/07/2017
+ * Time: 11:13
+ */
+
+function vd($_data) {
+  echo "<pre>";
+  var_dump($_data);
+  exit;
+}
