a0f13f13502f665272ce538ea7b2e40d109b2ceb
JordanBeziau <jordanbeziau@gmail.com>
2017-12-15 18:08:24 +0100

    Simulate trade api

--- React/trading/src/App.js
+++ React/trading/src/App.js
@@ -3,17 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    start_amount: null,
+    wallet: 100,
+    current_btc_value: null
+  }
 
   componentDidMount = () => {
-    setInterval(async () => {
-      const response = await fetch('https://bittrex.com/api/v1.1/public/getticker?market=BTC-LTC')
-      console.log('Response Bittrex', response)
-    }, 2000)
+    setInterval(() => {
+      const new_state = {...this.state}
+      const random_coin_value = Math.random() * (15000 - 9000) + 9000
+      new_state.current_btc_value = random_coin_value
+      if (this.state.start_amount === null) {
+        new_state.start_amount = random_coin_value
+        new_state.wallet -= .6 * this.state.wallet
+      } else if (this.state.start_amount < random_coin_value) {
+        new_state.wallet += .6 * this.state.wallet
+      }
+      this.setState(new_state)
+    }, 1000)
   }
 
   render() {
     return (
       <div className="App">
+        <p></p>
+        <p>WALLET : {this.state.wallet}</p>
+        <p>CURRENT VALUE : {this.state.current_btc_value}</p>
       </div>
     );
   }
