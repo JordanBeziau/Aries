4dc5aef4e4f638a15600962a71a672d53d5ee377
JordanBeziau <jordanbeziau@gmail.com>
2017-12-15 18:40:56 +0100

    setup link to route with react-router

--- React/router/src/App.js
+++ React/router/src/App.js
@@ -1,27 +1,66 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
-import LandingPage from './LandingPage'
-import AboutPage from './AboutPage'
-import NotFoundPage from './NotFoundPage'
-import logo from './logo.svg'
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import AboutPage from "./AboutPage";
+import NotFoundPage from "./NotFoundPage";
+import logo from "./logo.svg";
+import "./App.css";
+import { Menu, Grid } from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    activeItem: "home"
+  };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
+    const { activeItem } = this.state;
     return (
-      <div>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React-Router</h1>
-          </header>
-        </div>
-        <Router>
+      <Router>
+        <div>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React-Router</h1>
+              <Grid centered>
+                <Grid.Row>
+                  <Menu>
+                    <Link to="/">
+                      <Menu.Item
+                        as="span"
+                        name="home"
+                        active={activeItem === "home"}
+                        onClick={this.handleItemClick}
+                      >
+                        Home
+                      </Menu.Item>
+                    </Link>
+                    <Link to="/about">
+                      <Menu.Item
+                        as="span"
+                        name="about"
+                        active={activeItem === "about"}
+                        onClick={this.handleItemClick}
+                      >
+                        About
+                      </Menu.Item>
+                    </Link>
+                    <Link to="/lost">
+                      <Menu.Item
+                        as="span"
+                        name="lost"
+                        active={activeItem === "lost"}
+                        onClick={this.handleItemClick}
+                      >
+                        Lost
+                      </Menu.Item>
+                    </Link>
+                  </Menu>
+                </Grid.Row>
+              </Grid>
+            </header>
+          </div>
           <div>
             <Switch>
               <Route exact path="/" component={LandingPage} />
@@ -29,8 +68,8 @@ class App extends Component {
               <Route path="/*" component={NotFoundPage} />
             </Switch>
           </div>
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
