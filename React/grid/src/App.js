001f512bb6e93fc6d206d7a287eb1ac23bf3a6f8
JordanBeziau <jordanbeziau@gmail.com>
2017-12-15 10:59:06 +0100

    module size grid with inputs

--- React/grid/src/App.js
+++ React/grid/src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid, Input, Form, Label} from 'semantic-ui-react'
 
 class App extends Component {
+  state = {
+    nav: 'auto',
+    col1: 8,
+    col2: 8,
+    footer: 'auto'
+  }
+
+  handleChangeSize = (attr, event) => {
+    const new_size = {}
+    new_size[attr] = event.target.value
+    this.setState(new_size)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Grid>
+          <Grid.Column width={16}>
+          <Form>
+          <Label>NavBar 
+            <Input
+              type='text'
+              onChange={(event) => this.handleChangeSize('nav', event)}
+              value={this.state.nav}
+            />
+          </Label>
+          <Label>Column 
+            <Input
+              type='number'
+              onChange={(event) => this.handleChangeSize('col1', event)}
+              value={this.state.col1}
+            />
+          </Label>
+          <Label>Column 
+            <Input
+              type='number'
+              onChange={(event) => this.handleChangeSize('col2', event)}
+              value={this.state.col2}
+            />
+          </Label>
+          <Label>Footer 
+            <Input
+              type='text'
+              onChange={(event) => this.handleChangeSize('footer', event)}
+              value={this.state.footer}
+            />
+          </Label>
+          </Form>
+          </Grid.Column>
+        </Grid>
+        <Grid>
+          <Grid.Column width={16} textAlign="center" style={{background:"lightblue", height: this.state.nav}}>
+            NavBar
+          </Grid.Column>
+          <Grid.Column width={this.state.col1} textAlign="center" style={{background:"lightseagreen"}}>
+            Column
+          </Grid.Column>
+          <Grid.Column width={this.state.col2} textAlign="center" style={{background:"#819FF7"}}>
+            Column
+          </Grid.Column>
+          <Grid.Column width={16} textAlign="center" style={{background: '#FA5858', height: this.state.footer}} >
+            Footer
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
