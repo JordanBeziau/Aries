aea93982094a44667f40d51393d00352af1c2fdd
JordanBeziau <jordanbeziau@gmail.com>
2017-12-16 16:08:35 +0100

    adding gain for all

--- React/trading-api/src/App.js
+++ React/trading-api/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Statistic, Grid } from "semantic-ui-react";
+import GridColumn from "semantic-ui-react/dist/commonjs/collections/Grid/GridColumn";
 
 class App extends Component {
   state = {
     ask: null,
     bid: null,
     last: null,
-    gain: 0
+    gain_bid: 0,
+    gain_ask: 0,
+    gain_last: 0
   };
 
   _fetch = () => {
@@ -24,18 +27,28 @@ class App extends Component {
             ask: json.result.Ask,
             bid: json.result.Bid,
             last: json.result.Last,
-            gain: this.gainSetState(
-              this.state.gain,
+            gain_bid: this.gainSetState(
+              this.state.gain_bid,
               this.state.bid,
               json.result.Bid
+            ),
+            gain_ask: this.gainSetState(
+              this.state.gain_ask,
+              this.state.ask,
+              json.result.Ask
+            ),
+            gain_last: this.gainSetState(
+              this.state.gain_last,
+              this.state.last,
+              json.result.Last
             )
           });
         });
     }, 1000);
   };
 
-  gainSetState = (gain, bid, new_bid) => {
-    const new_gain = (new_bid - bid) / bid * 100;
+  gainSetState = (gain, value, new_value) => {
+    const new_gain = (new_value - value) / value * 100;
     return gain !== new_gain && new_gain !== 0 ? new_gain : gain;
   };
 
@@ -44,33 +57,68 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state);
     return (
       <div className="App">
         <h1>Bittrex</h1>
-        <Grid centered>
-          <Statistic.Group>
-            <Statistic>
-              <Statistic.Value>{this.state.bid}</Statistic.Value>
-              <Statistic.Label>Bid</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{this.state.ask}</Statistic.Value>
-              <Statistic.Label>Ask</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value
-                style={{ color: this.state.gain > 0 ? "green" : "red" }}
-              >
-                {Math.floor(this.state.gain * 10000000) / 10000000} %
-              </Statistic.Value>
-              <Statistic.Label>Gain</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>{this.state.last}</Statistic.Value>
-              <Statistic.Label>Last</Statistic.Label>
-            </Statistic>
-          </Statistic.Group>
+        <Grid divided="vertically" stackable>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value>{this.state.bid}</Statistic.Value>
+                <Statistic.Label>Bid</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value
+                  style={{ color: this.state.gain_bid > 0 ? "green" : "red" }}
+                >
+                  {Math.floor(this.state.gain_bid * 10000000) / 10000000} %
+                </Statistic.Value>
+                <Statistic.Label>Gain</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Grid divided="vertically" stackable>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value>{this.state.ask}</Statistic.Value>
+                <Statistic.Label>Ask</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value
+                  style={{ color: this.state.gain_ask > 0 ? "green" : "red" }}
+                >
+                  {Math.floor(this.state.gain_ask * 10000000) / 10000000} %
+                </Statistic.Value>
+                <Statistic.Label>Gain</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Grid divided="vertically" stackable>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value>{this.state.last}</Statistic.Value>
+                <Statistic.Label>Last</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic size="small">
+                <Statistic.Value
+                  style={{ color: this.state.gain_last > 0 ? "green" : "red" }}
+                >
+                  {Math.floor(this.state.gain_last * 10000000) / 10000000} %
+                </Statistic.Value>
+                <Statistic.Label>Gain</Statistic.Label>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </div>
     );
