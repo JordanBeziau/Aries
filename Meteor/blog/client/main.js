d433c9f616ccb735b6fb8abdd6215f163234fe51
JordanBeziau <jordanbeziau@gmail.com>
2018-01-04 11:13:13 +0100

    App component

--- Meteor/blog/client/main.js
+++ Meteor/blog/client/main.js
@@ -2,6 +2,8 @@ import { Meteor } from "meteor/meteor";
 import React from "react";
 import { render } from "react-dom";
 
+import App from "/imports/startup/App";
+
 Meteor.startup(() => {
-  render(<h1>Hello World !</h1>, document.getElementById("root"));
+  render(<App />, document.getElementById("root"));
 });
