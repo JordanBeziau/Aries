2adc29d7c4b4f029f5a6cf5bf456af38b7ba623f
JordanBeziau <jordanbeziau@gmail.com>
2018-01-04 12:32:29 +0100

    add form to insert page data

--- Meteor/blog/imports/pages/general/Landing.js
+++ Meteor/blog/imports/pages/general/Landing.js
@@ -1,21 +1,50 @@
 import React, { Component } from "react";
 
 export default class extends Component {
-  create_page = () => {
-    const new_page = {
-      title: "Ma première page",
-      description: "Ma première page enregistrée dans la BDD",
-      active: true,
-      created_at: new Date()
-    };
-    Meteor.call("dynamic_pages.insert", new_page);
+  state = {
+    title: "",
+    description: ""
+  };
+
+  handleChange = (attr, event) => {
+    const new_state = { ...this.state };
+    new_state[attr] = event.target.value;
+    this.setState(new_state);
+  };
+
+  create_page = event => {
+    event.preventDefault();
+    Meteor.call("dynamic_pages.insert", this.state, (error, result) => {
+      if (error) {
+        alert("Erreur de création de page : " + error);
+      } else {
+        console.log("Nouvelle page ajoutée");
+      }
+    });
   };
 
   render() {
+    const { title, description } = this.state;
     return (
       <div>
         <h1>Landing</h1>
-        <button onClick={this.create_page}>Créer une page</button>
+        <form onSubmit={this.create_page}>
+          <input
+            type="text"
+            value={title}
+            onChange={e => this.handleChange("title", e)}
+            placeholder="Title"
+          />
+          <input
+            type="text"
+            value={description}
+            onChange={e => this.handleChange("description", e)}
+            placeholder="Description"
+          />
+          <button>Créer une page</button>
+        </form>
+        <pre>{this.state.title}</pre>
+        <pre>{this.state.description}</pre>
       </div>
     );
   }
