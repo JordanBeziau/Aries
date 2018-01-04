15f50b18daa18aedfb51a45e3da1fc8aedd33e0a
JordanBeziau <jordanbeziau@gmail.com>
2018-01-04 15:53:28 +0100

    add react-router + NotFound component

--- Meteor/blog/imports/api/dynamic_pages/dynamic_pages.js
+++ Meteor/blog/imports/api/dynamic_pages/dynamic_pages.js
@@ -1,3 +1,22 @@
 import { Mongo } from "meteor/mongo";
 
 export const DynamicPages = new Mongo.Collection("dynamic_pages");
+
+const DynamicPagesSchema = new SimpleSchema({
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  active: {
+    type: Boolean,
+    defaultValue: true
+  },
+  created_at: {
+    type: Date,
+    defaultValue: new Date()
+  }
+});
+
+DynamicPages.attachSchema(DynamicPagesSchema);
--- Meteor/blog/imports/startup/App.js
+++ Meteor/blog/imports/startup/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Landing from "/imports/pages/general/Landing";
+import NotFound from "/imports/pages/general/NotFound";
 
 export default class extends Component {
   render() {
-    return <Landing />;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    );
   }
 }
